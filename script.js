daffd4ad94709dbbe0503416c28bf18542265664
JIAMIN ZENG <7930457-fatallium@users.noreply.replit.com>
2024-09-10 14:00:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function navigatePage(page) {
+  switch (page) {
+    case 0:
+      window.location.href = "index.html";
+      break;
+    case 1:
+      window.location.href = "educationSystem.html";
+      break;
+    case 2:
+      window.location.href = "writingAndReading.html";
+      break;
+    default:
+      window.location.href = "technology.html";
+      break;
+
+  }
+}
+
+// #89B6A5
+// #757083
